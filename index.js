51905fa1c7c9d2254dcdf92f6ad7678be2e4908c
Ravi <kkiran.ror@gmail.com>
2020-03-14 03:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function breakOut(array, changeValue, stopValue) {
-
+ array.forEach((ele)=>{
+   if (ele == changeValue){
+     break;
+   };
+ })
+return array
 }
 
 function keepGoing(array, changeValue, skipValue) {
