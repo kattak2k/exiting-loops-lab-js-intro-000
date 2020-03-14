adb9b637c68de50dabb986b5106b29b1f18645a3
Ravi <kkiran.ror@gmail.com>
2020-03-14 04:34:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
-    if (criteriaFunc(array[i])) {
+    if (findFn(array[i])) {
       return array[i]
     }
   }
