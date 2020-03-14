58e11b95cee730b5dcbf759ad8f804523530d71c
Ravi <kkiran.ror@gmail.com>
2020-03-14 04:33:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,5 +22,9 @@ function keepGoing(array, changeValue, skipValue) {
  }
 
 function findBy(array, findFn) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (criteriaFunc(array[i])) {
+      return array[i]
+    }
+  }
 }
